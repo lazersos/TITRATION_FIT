a2a1af3c25b5cec181096a361b9b0e0e9599308a
Samuel Lazerson <lazersos@gmail.com>
2023-06-12 09:18:41 +0200

    Now calculates area

--- titration_fit.py
+++ titration_fit.py
@@ -21,7 +21,15 @@ class TITRATION():
 		self.xdata = None
 		self.ydata = None
 		self.fit_func = lambda x,a,b,c,d,e:0.5*a*(1.0-np.tanh((x-b)/c))+d*x+e
+		# d/dx tahn = 1/cosh2 = 1-tanh2
+		self.fit_func_slope = lambda x,a,b,c,d,e:-0.5*a*(1.0/(np.cosh((x-b)/c))**2)/c+d
+		self.slope_func = lambda x,a,b:a*x+b
+		self.slope_func_integrand = lambda x,a,b:0.5*a*x*x+b*x
 		self.coefs = None
+		self.x0_tp = None
+		self.y0_tp = None
+		self.slope_tp = None
+		self.ycept_tp = None
 		self.XLabel = 'Volume [mL]'
 		self.YLabel = 'Potential [mV]'
 
@@ -108,15 +116,127 @@ class TITRATION():
 		popt, pcov = curve_fit(self.fit_func, self.xdata, self.ydata,p0=p0,bounds=bounds)
 		self.coefs = popt
 
+	def calcTitrationPoint(self):
+		""" Sets the titration point
+
+		Returnst the titration point and slope from the dataset
+
+		"""
+		# Get the derivative information
+		x = np.linspace(0,max(self.xdata)*1.2,1000)
+		fprime = self.fit_func_slope(x,*self.coefs)
+		self.slope_tp = min(fprime) # slope is negative so minimum value
+		maxindex = list(fprime).index(self.slope_tp)
+		self.x0_tp = x[maxindex]
+		self.y0_tp = self.fit_func(self.x0_tp,*self.coefs)
+		self.ycept_tp  = self.y0_tp - self.x0_tp*self.slope_tp
+
+	def getPerpLine(self,m,x0,y0):
+		""" Get the titration point
+
+		Returnst the titration point and slope from the dataset
+
+		Parameters
+		----------
+		m  : real
+			Slope of the titration line
+		x0 : real
+			X location of the titration point
+		y0 : real
+			Y location of the titration point
+
+		Returns
+		-------
+		m  : real
+			Slope of the titration curve at (X0,Y0)
+		b  : real
+			Y-intercept of line passing through (X0,Y0) with slope m
+		"""
+		# Normal is -1/dfdx
+		m = -1.0/m
+		b = y0 - x0*m
+		return m,b
+
+	def calcTangentLines(self):
+		""" Calculates the paramteters for the tangent lines
+		"""
+		fact = 2
+		x = self.x0_tp-self.coefs[2]*fact
+		y = self.fit_func(x,*self.coefs)
+		m = self.fit_func_slope(x,*self.coefs)
+		b = y - x*m
+		self.x1_tan = x
+		self.y1_tan = y
+		self.slope1_tan = m
+		self.ycept1_tan = b 
+		x = self.x0_tp+self.coefs[2]*fact
+		y = self.fit_func(x,*self.coefs)
+		m = self.fit_func_slope(x,*self.coefs)
+		b = y - x*m
+		self.x2_tan = x
+		self.y2_tan = y
+		self.slope2_tan = m
+		self.ycept2_tan = b
+
+	def calcAreas(self):
+		""" Calculates the area of the triangles
+		"""
+		import scipy.integrate as integrate
+		# First calcualte the points where the curves meet
+		[m2,b2] = self.getPerpLine(self.slope_tp,self.x0_tp,self.y0_tp)
+		xr    = (b2-self.ycept1_tan)/(self.slope1_tan-m2)
+		xl    = (b2-self.ycept2_tan)/(self.slope2_tan-m2)
+		# Now calculate the areas
+		result1 = integrate.quad(lambda x: self.slope_func(x,m2,b2), xl, self.x0_tp)
+		result2 = integrate.quad(lambda x: self.slope_func(x,self.slope2_tan,self.ycept2_tan), xl, self.x0_tp)
+		self.areal = result1[0]-result2[0]
+		result1 = integrate.quad(lambda x: self.slope_func(x,m2,b2), self.x0_tp, xr)
+		result2 = integrate.quad(lambda x: self.slope_func(x,self.slope1_tan,self.ycept1_tan), self.x0_tp, xr)
+		self.arear = result2[0]-result1[0]
+		# Now get the centerpoints of the triangles
+		yl = self.slope2_tan*xl+self.ycept2_tan
+		yr = self.slope1_tan*xr+self.ycept1_tan
+		self.x0_al = xl + (self.x0_tp-xl)/3.0
+		self.y0_al = yl - (self.y0_tp-yl)/3.0
+		self.x0_ar = xr + (self.x0_tp-xr)/3.0
+		self.y0_ar = yr - (self.y0_tp-yr)/3.0
+
+
+
+
+
 	def makePlot(self):
 		""" Plot the data
 
 		Creates a plot of the data
 		"""
 		import matplotlib.pyplot as plt
+		# Plot Dataset
 		plt.plot(self.xdata, self.ydata, 'ko', label='Data')
+		# Create an x array for plotting  and plot fit function
 		x2 = np.linspace(0,max(self.xdata)*1.2,1000)
 		plt.plot(x2,self.fit_func(x2, *self.coefs), 'r-', label='Fit: a=%5.3f, b=%5.3f, c=%5.3f, d=%5.3f, e=%5.3f' % tuple(self.coefs))
+		# Plot the titration point
+		plt.plot(self.x0_tp, self.y0_tp, 'bo', label='Titration Point (%5.3f,%5.3f)' % (self.x0_tp,self.y0_tp))
+		# Plot the tripple point normal line
+		[m2,b2] = self.getPerpLine(self.slope_tp,self.x0_tp,self.y0_tp)
+		plt.plot(x2,self.slope_func(x2,m2,b2),'c:')
+		# Plot the tangent lines
+		plt.plot(x2,self.slope1_tan*x2+self.ycept1_tan, 'b:')
+		plt.plot(x2,self.slope2_tan*x2+self.ycept2_tan, 'b:')
+		# Annotations
+		plt.annotate('%5.3f [mV*mL]' % (self.areal),
+            xy=(self.x0_al, self.y0_al), xycoords='data',
+            xytext=(-25, 45), textcoords='offset points',
+            arrowprops=dict(facecolor='black', shrink=0.05),
+            horizontalalignment='right', verticalalignment='bottom')
+		plt.annotate('%5.3f [mV*mL]' % (self.arear),
+            xy=(self.x0_ar, self.y0_ar), xycoords='data',
+            xytext=(85, -45), textcoords='offset points',
+            arrowprops=dict(facecolor='black', shrink=0.05),
+            horizontalalignment='right', verticalalignment='bottom')
+		# Cleanup the plot
+		plt.ylim(bottom=0,top=1.4*max(self.ydata))
 		plt.xlabel(self.XLabel)
 		plt.ylabel(self.YLabel)
 		plt.legend()
@@ -143,6 +263,9 @@ if __name__ == "__main__":
 	FIT.setFileName(args.filename)
 	FIT.readFile()
 	FIT.fitData()
+	FIT.calcTitrationPoint()
+	FIT.calcTangentLines()
+	FIT.calcAreas()
 	FIT.makePlot()
 	if (lverb): print('--------------------------------------')
 	sys.exit(0)
